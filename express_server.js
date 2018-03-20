d66a78ca907df9e2464198c484ed053ac8e0ec4d
Morganyyu <morganyu120@hotmail.com>
2018-03-20 21:32:11 +0000

    included _header.ejs in urls_index.ejs and urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -14,7 +14,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
